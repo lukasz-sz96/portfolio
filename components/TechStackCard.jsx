2aa24fefed9f18eddd150620a5742554c4dbec0d
lukasz-sz96 <lukasz-sz96@gmail.com>
2021-03-11 23:36:23 +0100

    Initial tech stack card (todo)

---
+++ components/TechStackCard.jsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+const TechStackCard = () => {
+    const spanClasses = "block text-center border-b m-3"
+  return (
+    <div className="flex flex-col flex-wrap w-full p-6 relative">
+      <h2 className="text-center text-4xl">I'm using</h2><br />
+      <div>
+      <span className={spanClasses}>Node.js</span>
+      <span className={spanClasses}>React.js</span>
+
+      <span className={spanClasses}>Next.js</span>
+
+      <span className={spanClasses}>Express.js</span>
+      <span className={spanClasses}>MySQL</span>
+      <span className={spanClasses}>MongoDB</span>
+
+
+      </div>
+    </div>
+  );
+};
+
+export default TechStackCard;
