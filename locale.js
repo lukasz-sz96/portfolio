52ba1a1f6aeda34d6487453727548b7a4f46681e
Kalmar <froozonezks@gmail.com>
2021-03-13 21:25:04 +0100

    Add paragraphs to locale config

--- locale.js
+++ locale.js
@@ -3,12 +3,13 @@ const locale = {
     title: [
       "Cześć! Jestem",
       "Łukasz",
-      "Jestem FrontEnd Developerem aspirującym do osiągnięcia statusu Full Stack Developera.",
+      "Jestem FrontEnd Developerem",
       "Naciśnij tę kartę, by poznać mnie lepiej.",
     ],
     titleDescription: [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mattis ullamcorper velit sed ullamcorper morbi tincidunt. Condimentum lacinia quis vel eros donec ac.",
-      "Consectetur purus ut faucibus pulvinar elementum. Quis risussed vulputate odio ut. Leo vel fringilla est ullamcorper eget nulla facilisi etiam dignissim. Faucibus pulvinar elementum integer enim neque volutpat. Nibh ipsum consequat nisl vel pretium lectus quam. Risus feugiat in ante metus dictum at.",
+      "Od wielu lat moją pasją jest tworzenie rozwiązań i aplikacji z obszaru Front End. Moim priorytetem jest dalsze rozwijanie się  w tej domenie, przede wszystkim z wykorzystaniem biblioteki React.js i jej pochodnych. W przyszłości zamierzam osiągnąć status Full Stack developera kontynuując poszerzanie wiedzy  i doświadczenia z technologiami Back End takimi jak Node.js.",
+      "Od 2017 roku prowadzę serwis społecznościowy o tematyce gier komputerowych. Tworzę tryby gier online w językach Lua i Javascript, włączając w to graficzne interfejsy budowane za pomocą React'a.",
+      "Od 2019 roku wykonuję zlecenia jako freelancer. Moje działanie opiera się głównie na tworzeniu i edycji skryptów JS i Lua oraz na konfigurowaniu serwerów dedykowanych, VPS i baz danych."
     ],
     imUsing: "Używam",
     contactMe: "Kontakt",
@@ -20,12 +21,13 @@ const locale = {
     title: [
       "Hi! I'm",
       "Łukasz",
-      "I'm a FrontEnd Developer aspiring to become a Full Stack Developer.",
+      "I'm a FrontEnd Developer.",
       "Click this card to get to know me better.",
     ],
     titleDescription: [
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mattis ullamcorper velit sed ullamcorper morbi tincidunt. Condimentum lacinia quis vel eros donec ac.",
-      "Consectetur purus ut faucibus pulvinar elementum. Quis risussed vulputate odio ut. Leo vel fringilla est ullamcorper eget nulla facilisi etiam dignissim. Faucibus pulvinar elementum integer enim neque volutpat. Nibh ipsum consequat nisl vel pretium lectus quam. Risus feugiat in ante metus dictum at.",
+      "For many years I’ve been passionate about creating Front End solutions and applications. Further development in that field is my top priority, especially with the use of React.js library and it’s derivatives. In the future, I plan to broaden my knowledge and experience with Back End technologies such as Node.js and become a Full Stack developer.",
+      "Since 2017 I'm managing a gaming-focuse social network. I'm creating game modes for online games with Lua and Javascript. That includes graphical user interfaces built with React.",
+      "Since 2019 I work as a freelancer. I mainly develop and edit JS and Lua scripts, and configure dedicated servers, VPSs and databases."
     ],
     imUsing: "I'm using",
     contactMe: "Contact me",
