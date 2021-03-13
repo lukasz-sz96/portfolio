dcf4a4b62b02b0c7286fc9cfb3584e4d9823cd91
Kalmar <froozonezks@gmail.com>
2021-03-13 22:07:01 +0100

    Fix text styling. Again

--- tailwind.config.js
+++ tailwind.config.js
@@ -193,6 +193,7 @@ module.exports = {
     },
     fontSize: {
       xs: ['0.75rem', { lineHeight: '1rem' }],
+      "1/2sm": ['0.8rem', {lineHeight: '1.12'}],
       sm: ['0.875rem', { lineHeight: '1.25rem' }],
       base: ['1rem', { lineHeight: '1.5rem' }],
       lg: ['1.125rem', { lineHeight: '1.75rem' }],
