0ae99ac6113701b04195288333590f0c3ba18bbc
Kalmar <froozonezks@gmail.com>
2021-03-13 21:26:36 +0100

    Animate box width

--- components/Card.jsx
+++ components/Card.jsx
@@ -13,7 +13,7 @@ const Card = ({
   red,
   scroll,
   isMobile,
-  language
+  language,
 }) => {
   const [isHovering, setIsHovering] = useState(false);
 
@@ -75,13 +75,19 @@ const Card = ({
       whileHover={animateHover && { scale: 1.03 }}
       transition={{ duration: 0.2 }}
       exit={{ opacity: 0, scale: 0.5 }}
-      className={`${gradient && "gradient"} md:w-${size} h-${
+      className={`${gradient && "gradient"} min-w-1/2 transition-all md:w-${size} h-${
         small ? "36" : "96"
       } ${
         red ? "bg-red-700" : "bg-gray-700"
       } mb-4 md:m-3 text-left text-white flex flex-row flex-wrap`}
     >
-      <Link href={{pathname: `${href ? href : ""}`, query: { lang: language ? language : "en"}}} scroll={scroll}>
+      <Link
+        href={{
+          pathname: `${href ? href : ""}`,
+          query: { lang: language ? language : "en" },
+        }}
+        scroll={scroll}
+      >
         <div
           className={`flex flex-row w-full transition-all ${
             gradient ? "h-full" : "h-auto"
@@ -91,12 +97,11 @@ const Card = ({
         </div>
       </Link>
       {!isMobile
-        ? isHovering && ( //TODO: For some reason || conditionals didn't work? Make me less ugly.
+        ? isHovering && //TODO: For some reason || conditionals didn't work? Make me less ugly.
           source && (
             <div className="flex flex-col flex-wrap lg:flex-row w-2/3 lg:w-1/2 relative lg:absolute top-1/3 lg:top-1/2 right-1/2 transform translate-x-full lg:translate-x-1/2 -translate-y-1/2 z-50">
               <Options />
             </div>
-          )
           )
         : source && (
             <div className="flex flex-col flex-wrap lg:flex-row w-2/3 lg:w-1/2 relative lg:absolute top-1/3 lg:top-1/2 right-1/2 transform translate-x-full lg:translate-x-1/2 -translate-y-1/2 z-50">
